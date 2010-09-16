b5b344ebd76c564197c32e4cc2ba3f6fe75aa260
Salvador <sfandino@yahoo.com>
2010-09-16 12:22:07 +0200

    update sample script to use new stderr_fh feature

--- samples/capture_stderr.pl
+++ samples/capture_stderr.pl
@@ -5,27 +5,18 @@ use warnings;
 
 use Net::SFTP::Foreign;
 use File::Temp;
-use IPC::Open3;
-use IPC::Open2;
-use Fcntl qw(:mode O_NONBLOCK F_SETFL F_GETFL);
 
-my $hostname = 'localhost';
+my $hostname = shift // 'localhost';
 
-my $ssherr = File::Temp->new
-    or die "tempfile failed";
+my $ssherr = File::Temp->new or die "tempfile failed";
 
-open my $stderr_save, '>&STDERR' or die "unable to dup STDERR";
-open STDERR, '>&'.fileno($ssherr);
-
-my $sftp = Net::SFTP::Foreign->new($hostname, more => qw(-v));
-
-open STDERR, '>&'.fileno($stderr_save);
+my $sftp = Net::SFTP::Foreign->new($hostname, more => qw(-v), stderr_fh => $ssherr);
 
 if ($sftp->error) {
   print "sftp error: ".$sftp->error."\n";
   seek($ssherr, 0, 0);
   while (<$ssherr>) {
-    print "error: $_";
+    print " ssh error: $_";
   }
 }
 
