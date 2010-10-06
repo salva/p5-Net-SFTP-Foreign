7bda4150d6af963d39aa603ec552a079a4ca7a16
Salvador <sfandino@yahoo.com>
2010-10-06 17:09:11 +0200

    add sample for Log::WarnDie

---
+++ samples/log_warndie.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+use Net::SFTP::Foreign;
+
+use Log::WarnDie;
+# Log::WarnDie is missing FILENO method, just patch it in...
+sub Log::WarnDie::FILENO { -1 }
+
+use Log::Dispatch;
+use Log::Dispatch::File;
+
+my $dispatcher = Log::Dispatch->new;
+$dispatcher->add(Log::Dispatch::File->new(name => 'foo',
+                                          min_level => 'debug',
+                                          filename => "/tmp/dispatcher.log"));
+Log::WarnDie->dispatcher( $dispatcher );
+
+my $s = Net::SFTP::Foreign->new("localhost");
+$s->error and die "unable to connecte to remote host: " . $s->error;
+print Dumper $s->ls('.', names_only => 1);
+
+
