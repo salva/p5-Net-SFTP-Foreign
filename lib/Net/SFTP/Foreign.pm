d7828e657dacea13ce710303274e36aedf2b24ee
Salvador <sfandino@yahoo.com>
2012-02-06 17:16:55 +0100

    add support for put_content method

--- lib/Net/SFTP/Foreign.pm
+++ lib/Net/SFTP/Foreign.pm
@@ -376,7 +376,7 @@ sub _get_msg_and_check {
 	    else {
 		$sftp->_conn_lost(SSH2_FX_BAD_MESSAGE,
 				  SFTP_ERR_REMOTE_BAD_MESSAGE,
-				  $errstr, "bad packet type, expected $etype packet, got $type");	
+				  $errstr, "bad packet type, expected $etype packet, got $type");
 	    }
 	    return undef;
 	}
@@ -2195,6 +2195,24 @@ sub put {
         }
     };
     $sftp->_ok_or_autodie;
+}
+
+sub put_content {
+    @_ >= 3 or croak 'Usage: $sftp->put_content($content, $remote, %opts)';
+    ${^TAINT} and &_catch_tainted_args;
+
+    my ($sftp, undef, $remote, %opts) = @_;
+
+    my %put_opts = ( map { $_ => delete $opts{$_} }
+                     qw(perm block_size queue_size overwrite conversion resume
+                        numbered late_set_perm atomic best_effort));
+
+    unless (open my $fh, '<', \$_[0]) {
+        $sftp->_set_error(SFTP_ERR_LOCAL_OPEN_FAILED, "Can't open scalar as file handle", $!);
+        return undef;
+    }
+    $sftp->put($fh, $remote, %opts);
+
 }
 
 sub ls {
