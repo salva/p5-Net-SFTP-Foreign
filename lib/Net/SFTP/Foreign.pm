f5ae70822261f11c75af0a775246941ddc7a9968
Salvador <sfandino@yahoo.com>
2012-02-06 18:06:44 +0100

    solve error un put_content

--- lib/Net/SFTP/Foreign.pm
+++ lib/Net/SFTP/Foreign.pm
@@ -2206,12 +2206,13 @@ sub put_content {
     ${^TAINT} and &_catch_tainted_args;
 
     my ($sftp, undef, $remote, %opts) = @_;
-
     my %put_opts = ( map { $_ => delete $opts{$_} }
                      qw(perm umask block_size queue_size overwrite conversion resume
                         numbered late_set_perm atomic best_effort));
+    %opts and _croak_bad_options(keys %opts);
 
-    unless (open my $fh, '<', \$_[0]) {
+    my $fh;
+    unless (CORE::open $fh, '<', \$_[0]) {
         $sftp->_set_error(SFTP_ERR_LOCAL_OPEN_FAILED, "Can't open scalar as file handle", $!);
         return undef;
     }
