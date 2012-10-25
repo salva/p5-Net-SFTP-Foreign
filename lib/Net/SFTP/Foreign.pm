7d3e509eb4a66f5071da8081533607d7e7ae0817
Salvador Fandino <sfandino@yahoo.com>
2012-10-25 20:13:38 +0200

    add support for local encoding into put_symlink method

--- lib/Net/SFTP/Foreign.pm
+++ lib/Net/SFTP/Foreign.pm
@@ -2547,13 +2547,14 @@ sub put_symlink {
     my ($sftp, $local, $remote, %opts) = @_;
     my $overwrite = delete $opts{overwrite};
     my $numbered = delete $opts{numbered};
+    my $local_encoded = $sftp->_local_fs_encode($local);
 
     croak "'overwrite' and 'numbered' can not be used together"
 	if ($overwrite and $numbered);
     %opts and _croak_bad_options(keys %opts);
 
     $overwrite = 1 unless (defined $overwrite or $numbered);
-    my $perm = (CORE::lstat $local)[2];
+    my $perm = (CORE::lstat $local_encoded)[2];
     unless (defined $perm) {
 	$sftp->_set_error(SFTP_ERR_LOCAL_STAT_FAILED,
 			  "Couldn't stat local file '$local'", $!);
@@ -2570,6 +2571,7 @@ sub put_symlink {
 			  "Couldn't read link '$local'", $!);
 	return undef;
     }
+    $target = $sftp->_local_fs_decode($target);
 
     while (1) {
         local $sftp->{_autodie};
