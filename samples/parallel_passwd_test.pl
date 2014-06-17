a523f02b1c5e81ce7015d3708b32903e01286288
Salvador Fandino <sfandino@yahoo.com>
2014-06-17 15:07:11 +0200

    check several processes trying to log in parallel

---
+++ samples/parallel_passwd_test.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use 5.010;
+use Net::SFTP::Foreign;
+
+my $n = shift @ARGV // 10;
+my $target = shift @ARGV // 'localhost';
+my $password = shift @ARGV // 'foo';
+
+my @pid;
+$| = 1;
+
+$Net::SFTP::Foreign::debug = ~0;
+#$IO::Tty::DEBUG = 1;
+
+use POSIX;
+
+for my $i (1..$n) {
+    my $pid = fork;
+    unless ($pid) {
+        unless (defined $pid) {
+            warn "fork $i failed: $!";
+            next;
+        }
+
+        my $strace;
+        my $c = 0;
+        OUT: while (1) {
+            open STDERR, ">", "out.$i.$c";
+            $c++;
+            while (1) {
+                warn "creating connection for worker $i";
+                print STDOUT ".";
+                my $sftp = Net::SFTP::Foreign->new($target, password => $password, more => '-vv');
+                if ($sftp->error) {
+                    warn "new failed: " . $sftp->error;
+                    print STDOUT "worker $i failed\n";
+                    # system "strace -f -o /tmp/strace.$i.out -p $$ &" unless $strace++;
+                    next OUT;
+                }
+            }
+        }
+    }
+
+    warn "process $pid forked for worker $i";
+
+    push @pid, $pid;
+}
+
+1 while wait();
+
+END { kill TERM => @pid; }
